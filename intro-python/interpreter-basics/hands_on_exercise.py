d36685e4d8c225cc5ddc8324187d1ef65f6f4125
xmoose101x <72230682+xmoose101x@users.noreply.github.com>
2022-08-19 16:41:34 -0500

    Hands On Exercise

--- intro-python/interpreter-basics/hands_on_exercise.py
+++ intro-python/interpreter-basics/hands_on_exercise.py
@@ -28,24 +28,35 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print("pi is of type {} and value {}".format(type(pi), pi))
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+if i < 50:
+    print(f"{i} is less than 50")
 
+elif i > 50:
+    print(f"{i} is greater than 50")
 
 # TODO: Write a conditional that prints the color of the selected sportsball
 picked_sportsball = random.choice(['tennis', 'basketball', 'golf'])
-
+if picked_sportsball == "tennis":
+    print("The color of tennis is green")
+elif picked_sportsball == "basketball":
+    print("The color of basketball is orange")
+elif picked_sportsball == "golf":
+    print("The color of golf is white")
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def multiply(num1, num2):
+    return num1 * num2
 
 # TODO: Now call the function a few times to calculate the following answers
 
-print("12 x 96 =",)
+print("12 x 96 =", multiply(12, 96))
 
-print("48 x 17 =",)
+print("48 x 17 =", multiply(48, 17))
 
-print("196523 x 87323 =",)
+print("196523 x 87323 =", multiply(196523, 87323))
